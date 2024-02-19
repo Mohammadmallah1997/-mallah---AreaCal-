3c51ecb4662e087ee2f9d426d21f3ef01d6ce856
User <User@MOHAMMAD-MALLAH>
2024-02-19 14:54:38 +0200

    Add project files.

---
+++ areaCal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Rectangle Area Calculator");
+
+        Console.Write("Enter the length of the rectangle: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the width of the rectangle: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        double area = length * width;
+        Console.WriteLine($"The area of the rectangle is: {area}");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
